edd9dae8e24294b64243e5042653383638cd0fe4
Max Campman <max@campman.xyz>
2018-05-13 21:23:53 +0100

    Basic GUI

---
+++ src/main/java/uk/ac/cam/cl/gui/Widget.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cl.gui;
+
+import javafx.scene.layout.GridPane;
+
+public class Widget extends GridPane {
+    public Widget() {
+        super();
+        this.getStyleClass().add("widget");
+    }
+}
