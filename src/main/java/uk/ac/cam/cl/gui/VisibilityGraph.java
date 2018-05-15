50f510a90537f6bb44d483c5f808fc11f2c06956
Ben Cole <bencole12345@gmail.com>
2018-05-15 16:34:10 +0100

    Add visibility graph

---
+++ src/main/java/uk/ac/cam/cl/gui/VisibilityGraph.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.cl.gui;
+
+import uk.ac.cam.cl.data.DataPoint;
+
+public class VisibilityGraph extends GraphWidget {
+
+    @Override
+    protected String getChartTitle() {
+        return "Visibility";
+    }
+
+    @Override
+    protected double getRelevantData(DataPoint dataPoint) {
+        return dataPoint.getVisibilityMiles();
+    }
+}
