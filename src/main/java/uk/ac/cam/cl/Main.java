ce202b9bbc4346d69608fc927e1e1e06076ff3c2
Max Campman <max@campman.xyz>
2018-05-16 20:42:07 +0100

    Addition of basic styling

--- src/main/java/uk/ac/cam/cl/Main.java
+++ src/main/java/uk/ac/cam/cl/Main.java
@@ -7,12 +7,18 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.*;
+
+import org.json.simple.JSONArray;
+import uk.ac.cam.cl.data.AppSettings;
+import uk.ac.cam.cl.data.AppSettingsFailure;
 import uk.ac.cam.cl.data.DataManager;
 import uk.ac.cam.cl.gui.*;
 
@@ -20,14 +26,86 @@ public class Main extends Application {
     private Stage stage;
     private Scene mainScene = null;
     private Scene menuScene = null;
-    private Map<String,Widget> widgets = new HashMap<>();  
+    private Map<String,Widget> widgets = new HashMap<>();
+    private AppSettings settings = AppSettings.getInstance();
     // widgets to be added to screen mapping name => widget
 
     private static int NUM_OF_WIDGETS = 10;
 
+    private List<String> getWidgetList() {
+        List<String> wid = new ArrayList<>();
+
+        wid.add("wind");
+        wid.add("temp");
+        wid.add("visi");
+        wid.add("tide");
+
+        return wid;
+    }
+
+    private String nameToDesc(String w) {
+        String ret = null;
+        switch (w) {
+            case "wind":
+                ret = "Wind speed/direction";
+                break;
+
+            case "temp":
+                ret = "Temperature";
+                break;
+
+            case "visi":
+                ret = "Visibility";
+                break;
+
+            case "tide":
+                ret = "Tides";
+                break;
+        }
+        return ret;
+    }
+
+    private Widget stringToWidget(String w) {
+        Widget ret = null;
+        switch (w) {
+            case "wind":
+                ret = new WindWidget();
+                break;
+
+            case "temp":
+                ret = new TemperatureGraph();
+                break;
+
+            case "visi":
+                ret = new VisibilityGraph();
+                break;
+
+            case "tide":
+                ret = new TideGraph();
+                break;
+        }
+
+        return ret;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Thread.sleep(2000); // dirty hack as temp fix
         this.stage = primaryStage;
+        try {
+            JSONArray widgs = settings.get("widgets");
+            if (widgs == null) {
+                settings.set("widgets", new JSONArray());
+            }
+            widgs = settings.get("widgets");
+            for (int i=0; i<widgs.size(); i++) {
+                String w = (String) widgs.get(i);
+                widgets.put(w, stringToWidget(w));
+            }
+        } catch (AppSettingsFailure e) {
+            e.printStackTrace();
+            settings.set("widgets", new JSONArray());
+        }
         showMain();
     }
 
@@ -35,12 +113,11 @@ public class Main extends Application {
      * Show the main panel.
      * */
     public void showMain() {
+        ScrollPane scrollPane = new ScrollPane();
         BorderPane root = new BorderPane();
         GridPane topBar = new TopBar(this);
         GridPane mainSec = new GridPane();
         GridPane bottomBar = new BottomBar(this);
-
-        root.setPadding(new Insets(5));
         this.stage.setTitle("Five Seas");
 
         // add widgets to the panel
@@ -52,11 +129,17 @@ public class Main extends Application {
 
         root.setTop(topBar);
         root.setBottom(bottomBar);
-        root.setCenter(mainSec);
+        scrollPane.setContent(mainSec);
+        root.setCenter(scrollPane);
+        //root.setStyle("-fx-background-color: #ffffff;");
+        root.getStylesheets().add("style.css");
+
 
         this.mainScene = new Scene(root, 380, 675);
+        this.mainScene.getStylesheets().add("style.css");
         this.stage.setScene(this.mainScene);
         this.stage.show();
+
     }
 
     /**
@@ -72,40 +155,49 @@ public class Main extends Application {
         BorderPane root = new BorderPane();
         GridPane topBar = new MenuBar(this);
         GridPane mainSec = new GridPane();
+        mainSec.setId("menu-main");
 
-        
-        //Example check box for widget settings with an example of how to 
-        //add widgets to the main screen
-        CheckBox windWidgCheck = new CheckBox();
-        windWidgCheck.setText("Wind speed/direction");
-        windWidgCheck.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, 
-                    Boolean oldValue, Boolean newValue) {
-                if (newValue) {
-                    widgets.put("wind", new WindWidget());
-                } else {
-                    widgets.remove("wind");
+        List<CheckBox> checkBoxes = new ArrayList<>();
+        List<String> wids = getWidgetList();
+        for (int i=0; i<wids.size(); i++) {
+            final int x = i;
+            checkBoxes.add(new CheckBox());
+            checkBoxes.get(i).setText(nameToDesc(wids.get(i)));
+            checkBoxes.get(i).selectedProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> observable,
+                                    Boolean oldValue, Boolean newValue) {
+                    if (newValue) {
+                        System.out.println("added widget: " + wids.get(x));
+                        JSONArray widgs = settings.get("widgets");
+                        if (!widgs.contains(wids.get(x))) {
+                            widgs.add(wids.get(x));
+                        }
+                        settings.set("widgets", widgs);
+                        widgets.put(wids.get(x), stringToWidget(wids.get(x)));
+                    } else {
+                        System.out.println("removed widget: " + wids.get(x));
+                        JSONArray widgs = settings.get("widgets");
+                        while (widgs.contains(wids.get(x))) {
+                            widgs.remove(wids.get(x));
+                        }
+                        settings.set("widgets", widgs);
+                        widgets.remove(wids.get(x));
+                    }
                 }
+            });
+            JSONArray widgs = settings.get("widgets");
+            checkBoxes.get(i).setPadding(new Insets(5, 10, 5, 10));
+            checkBoxes.get(i).getStyleClass().add("check-box");
+            if (widgs.contains(wids.get(i))) {
+                checkBoxes.get(i).setSelected(true);
+            } else {
+                checkBoxes.get(i).setSelected(false);
             }
-        });
-        CheckBox tempWidgCheck = new CheckBox();
-        tempWidgCheck.setText("Temperature forecast");
-        tempWidgCheck.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable,
-                                Boolean oldValue, Boolean newValue) {
-                if (newValue) {
-                    widgets.put("temp", new TemperatureGraph());
-                } else {
-                    widgets.remove("temp");
-                }
-            }
-        });
-        mainSec.add(windWidgCheck, 0, 0);
-        mainSec.add(tempWidgCheck, 0, 1);
+            mainSec.add(checkBoxes.get(i), 0, i);
+        }
 
-        root.setPadding(new Insets(5));
+        root.setPadding(new Insets(0));
 
         this.stage.setTitle("Widget Menu");
 
@@ -113,6 +205,7 @@ public class Main extends Application {
         root.setCenter(mainSec);
 
         this.menuScene = new Scene(root, 380, 675);
+        this.menuScene.getStylesheets().add("style.css");
         this.stage.setScene(menuScene);
         this.stage.show();
     }
