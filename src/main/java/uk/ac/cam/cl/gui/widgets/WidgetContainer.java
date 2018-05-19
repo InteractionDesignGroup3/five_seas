192b602cff309ee7d5f98fb2a06be1078574958b
Max Campman <max@campman.xyz>
2018-05-19 21:22:39 +0100

    Fade transition to settings

--- src/main/java/uk/ac/cam/cl/gui/widgets/WidgetContainer.java
+++ src/main/java/uk/ac/cam/cl/gui/widgets/WidgetContainer.java
@@ -1,8 +1,16 @@
 package uk.ac.cam.cl.gui.widgets;
 
+import javafx.animation.FadeTransition;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.util.Set;
 
 /**
  * Wraps a Widget with a border.
@@ -12,6 +20,12 @@ import javafx.scene.layout.HBox;
 
 public class WidgetContainer extends BorderPane {
   private Integer position;
+  private boolean onMain = true;
+  private Widget main;
+  private Settings sett;
+  private BorderPane top;
+  private Button swipe;
+  private StackPane view;
 
   public WidgetContainer(Widget widget, Integer pos) {
     super();
@@ -23,6 +37,74 @@ public class WidgetContainer extends BorderPane {
     bottom.getChildren().addAll(nameLabel);
     setBottom(bottom);
     this.getStyleClass().add("widget_container");
+  }
+
+  public WidgetContainer(Widget widget, Settings sett, Integer pos) {
+
+    position = pos;
+    HBox bottom = new HBox();
+    Label nameLabel = new Label();
+    nameLabel.setText(getWidgetName(widget));
+    bottom.getChildren().addAll(nameLabel);
+    setBottom(bottom);
+    this.getStyleClass().add("widget_container");
+
+    //this(widget, pos);
+    this.top = new BorderPane();
+    this.swipe = new Button();
+    this.swipe.setText(">");
+
+    this.main = widget;
+    this.sett = sett;
+    this.view = new StackPane();
+    this.view.getChildren().addAll(this.sett, this.main);
+
+    this.top.setRight(this.swipe);
+
+    this.swipe.setOnAction((actionEvent) -> {
+      swap();
+    });
+
+    setCenter(view);
+    setTop(top);
+  }
+
+  private void swap() {
+
+    if (this.onMain) {
+      this.swipe.setText("<");
+      FadeTransition ft = new FadeTransition(Duration.millis(500), this.main);
+      ft.setFromValue(1.0);
+      ft.setToValue(0.0);
+      ft.setCycleCount(1);
+      ft.play();
+
+      FadeTransition ft2 = new FadeTransition((Duration.millis(500)), this.sett);
+      ft2.setFromValue(0.0);
+      ft2.setToValue(1.0);
+      ft2.setCycleCount(1);
+      ft2.play();
+    }
+    else {
+      FadeTransition ft = new FadeTransition(Duration.millis(500), this.sett);
+      ft.setFromValue(1.0);
+      ft.setToValue(0.0);
+      ft.setCycleCount(1);
+      ft.play();
+
+      FadeTransition ft2 = new FadeTransition((Duration.millis(500)), this.main);
+      ft2.setFromValue(0.0);
+      ft2.setToValue(1.0);
+      ft2.setCycleCount(1);
+      ft2.play();
+
+      this.swipe.setText(">");
+    }
+
+    Node first = view.getChildren().remove(0);
+    view.getChildren().add(first);
+
+    this.onMain = !this.onMain;
   }
 
   /**
