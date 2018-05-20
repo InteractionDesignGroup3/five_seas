c7a438372c8d5884425bba74fefa3512c45bcd9f
Nathan Corbyn <me@nathancorbyn.com>
2018-05-20 13:13:25 +0100

    Style check boxes

--- src/main/java/uk/ac/cam/cl/gui/widgets/WindWidget.java
+++ src/main/java/uk/ac/cam/cl/gui/widgets/WindWidget.java
@@ -24,14 +24,12 @@ import uk.ac.cam.cl.data.*;
  * @author Mike Cachopo
  */
 public class WindWidget extends Widget {
-
   public static final String WIND_WIDGET_UNIT_SETTINGS = "windWidgetUnit";
 
   private DataSequence dataSequence;
   private Label timeValue, gustSpeed, numberBox, north;
   private Shape compass, vane;
   private Slider timeSelecter;
-
 
   @Override
   public void initialise() {
